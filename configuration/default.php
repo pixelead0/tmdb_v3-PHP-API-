1744e96e64ce8ac3b1c455b9585cc0e076c54ab1
Bogdan Finn <bogdan@applike.info>
2020-10-25 13:15:05 +0100

    refs #70: implemented getTrailers() for TVShows and added example;

--- configuration/default.php
+++ configuration/default.php
@@ -12,8 +12,8 @@ $cnf['adult'] = false;
 $cnf['debug'] = false;
 
 // Data Return Configuration - Manipulate if you want to tune your results
-$cnf['appender']['movie'] = array('trailers', 'images', 'credits', 'translations', 'reviews');
-$cnf['appender']['tvshow'] = array('trailers', 'images', 'credits', 'translations', 'keywords');
+$cnf['appender']['movie'] = array('trailers', 'videos', 'images', 'credits', 'translations', 'reviews');
+$cnf['appender']['tvshow'] = array('trailers', 'videos', 'images', 'credits', 'translations', 'keywords');
 $cnf['appender']['season'] = array('trailers', 'images', 'credits', 'translations');
 $cnf['appender']['episode'] = array('trailers', 'images', 'credits', 'translations');
 $cnf['appender']['person'] = array('movie_credits', 'tv_credits', 'images');
